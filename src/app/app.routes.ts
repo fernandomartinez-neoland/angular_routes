38194cf58cef7d051d6cb597ff04b7b7a0ca2b2d
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-02-24 10:05:22 +0100

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: 'servicios',
+                component: ServiciosComponent
+            },
+            {
+                path:'contacto',
+                component:ContactoComponent
+            }
+        ]
+
+    }
+];
